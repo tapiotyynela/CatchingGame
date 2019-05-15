53ff8ab4023208e4d674a7128e144de94d76b34b
Tapio Tyynelä <tapio.tyynela@gmail.com>
2019-05-15 16:57:05 +0300

    Comments added and finished the game

--- CatchingGame.py
+++ CatchingGame.py
@@ -2,23 +2,28 @@ from itertools import cycle
 from random import randrange
 from tkinter import Canvas, Tk, messagebox, font
 
+
+
 canvas_width = 800
 canvas_height = 400
 
+#Luodaan pelinäyttö
 root = Tk()
 c = Canvas(root, width=canvas_width, height=canvas_height, background='deep sky blue')
 c.create_rectangle(-5, canvas_height - 100, canvas_width + 5, canvas_height + 5, fill='sea green', width=0)
 c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
 c.pack()
 
+#Luodaan muuttuja arvot munalle
 color_cycle = cycle(['light blue', 'light green', 'light pink', 'light yellow', 'light cyan'])
 egg_width = 45
 egg_height = 55
 egg_score = 10
 egg_speed = 500
 egg_interval = 4000
-difficulty_factor = 0,95
+difficulty_factor = 0.95
 
+#Luodaan muuttuja arvot siepparille
 catcher_color = 'red'
 catcher_width = 100
 catcher_height = 100
@@ -40,6 +45,7 @@ lives_text = c.create_text(canvas_width - 10, 10, anchor='ne', font=game_font, f
 
 eggs = []
 
+#Funktio jossa luodaan uusi muna
 def create_egg():
     x = randrange(10, 740)
     y = 40
@@ -47,6 +53,7 @@ def create_egg():
     eggs.append(new_egg)
     root.after(egg_interval, create_egg)
 
+#Funktio joka liikuttaa munia
 def move_eggs():
     for egg in eggs:
         (egg_x, egg_y, egg_x2, egg_y2) = c.coords(egg)
@@ -55,6 +62,7 @@ def move_eggs():
             egg_dropped(egg)
     root.after(egg_speed, move_eggs)
 
+#Funktio joka huomaa kun muna putoaa
 def egg_dropped(egg):
     eggs.remove(egg)
     c.delete(egg)
@@ -63,11 +71,13 @@ def egg_dropped(egg):
         messagebox.showinfo('Peli on ohi!', 'Final score: ' + str(score))
         root.destroy()
 
+#Funktio joka vähentää elämön munan pudotessa
 def lose_a_life():
     global lives_remaining
     lives_remaining -= 1
     c.itemconfigure(lives_text, text='Lives: ' + str(lives_remaining))
 
+#Funktio joka tarkastaa onko muna siepattu oikein
 def check_catch():
     (catcher_x, catcher_y, catcher_x2, catcher_y2) = c.coords(catcher)
     for egg in eggs:
@@ -78,6 +88,7 @@ def check_catch():
             increase_score(egg_score)
     root.after(100, check_catch)
 
+#Funktio joka kasvattaa pistemäärää
 def increase_score(points):
     global score, egg_speed, egg_interval
     score += points
@@ -85,6 +96,7 @@ def increase_score(points):
     egg_interval = int(egg_interval * difficulty_factor)
     c.itemconfigure(score_text, text='Score: ' + str(score))
 
+#Näissä funktioissa määritetään siepparin liikkuminen.
 def move_left(event):
     (x1, y1, x2, y2) = c.coords(catcher)
     if x1 > 0:
@@ -95,8 +107,8 @@ def move_right(event):
     if x2 < canvas_width:
         c.move(catcher, 20, 0)
 
-c.bind('<Left>', move_left())
-c.bind('<Right>', move_right())
+c.bind('<Left>', move_left)
+c.bind('<Right>', move_right)
 c.focus_set()
 
 
