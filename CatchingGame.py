9042e4d16e67d22167a2a4935e8089c45d12a5b8
Tapio Tyynelä <tapio.tyynela@gmail.com>
2019-05-15 12:03:57 +0300

    Created funktions and player movement

--- CatchingGame.py
+++ CatchingGame.py
@@ -38,4 +38,70 @@ score_text = c.create_text(10, 10, anchor='nw', font=game_font, fill='darkblue',
 lives_remaining = 3
 lives_text = c.create_text(canvas_width - 10, 10, anchor='ne', font=game_font, fill='darkblue', text='Lives: ' + str(lives_remaining))
 
+eggs = []
+
+def create_egg():
+    x = randrange(10, 740)
+    y = 40
+    new_egg = c.create_oval(x, y, x + egg_width, y + egg_height, fill=next(color_cycle), width=0)
+    eggs.append(new_egg)
+    root.after(egg_interval, create_egg)
+
+def move_eggs():
+    for egg in eggs:
+        (egg_x, egg_y, egg_x2, egg_y2) = c.coords(egg)
+        c.move(egg, 0, 10)
+        if egg_y2 > canvas_height:
+            egg_dropped(egg)
+    root.after(egg_speed, move_eggs)
+
+def egg_dropped(egg):
+    eggs.remove(egg)
+    c.delete(egg)
+    lose_a_life()
+    if lives_remaining == 0:
+        messagebox.showinfo('Peli on ohi!', 'Final score: ' + str(score))
+        root.destroy()
+
+def lose_a_life():
+    global lives_remaining
+    lives_remaining -= 1
+    c.itemconfigure(lives_text, text='Lives: ' + str(lives_remaining))
+
+def check_catch():
+    (catcher_x, catcher_y, catcher_x2, catcher_y2) = c.coords(catcher)
+    for egg in eggs:
+        (egg_x, egg_y, egg_x2, egg_y2) = c.coords(egg)
+        if catcher_x < egg_x and egg_x2 < catcher_x2 and catcher_y2 - egg_y2 < 40:
+            eggs.remove(egg)
+            c.delete(egg)
+            increase_score(egg_score)
+    root.after(100, check_catch)
+
+def increase_score(points):
+    global score, egg_speed, egg_interval
+    score += points
+    egg_speed = int(egg_speed * difficulty_factor)
+    egg_interval = int(egg_interval * difficulty_factor)
+    c.itemconfigure(score_text, text='Score: ' + str(score))
+
+def move_left(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x1 > 0:
+        c.move(catcher, -20, 0)
+
+def move_right(event):
+    (x1, y1, x2, y2) = c.coords(catcher)
+    if x2 < canvas_width:
+        c.move(catcher, 20, 0)
+
+c.bind('<Left>', move_left())
+c.bind('<Right>', move_right())
+c.focus_set()
+
+
+
+root.after(1000, create_egg)
+root.after(1000, move_eggs)
+root.after(1000, check_catch)
 root.mainloop()
