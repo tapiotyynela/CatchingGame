e2113906eecb06e9461dee547eaf9330abd0c6ff
Tapio Tyynelä <tapio.tyynela@gmail.com>
2019-05-15 11:30:30 +0300

    Added catcher to the game

--- CatchingGame.py
+++ CatchingGame.py
@@ -10,4 +10,24 @@ c = Canvas(root, width=canvas_width, height=canvas_height, background='deep sky 
 c.create_rectangle(-5, canvas_height - 100, canvas_width + 5, canvas_height + 5, fill='sea green', width=0)
 c.create_oval(-80, -80, 120, 120, fill='orange', width=0)
 c.pack()
+
+color_cycle = cycle(['light blue', 'light green', 'light pink', 'light yellow', 'light cyan'])
+egg_width = 45
+egg_height = 55
+egg_score = 10
+egg_speed = 500
+egg_interval = 4000
+difficulty_factor = 0,95
+
+catcher_color = 'red'
+catcher_width = 100
+catcher_height = 100
+catcher_start_x = canvas_width / 2 - canvas_width / 2
+catcher_start_y = canvas_height - catcher_height - 20
+catcher_start_x2 = catcher_start_x + catcher_width
+catcher_start_y2 = catcher_start_y + catcher_height
+
+catcher = c.create_arc(catcher_start_x, catcher_start_y, catcher_start_x2, catcher_start_y2,\
+                       start=200, extent=140, style = 'arc', outline=catcher_color, width=3)
+
 root.mainloop()
